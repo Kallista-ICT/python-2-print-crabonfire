e756062c386df9a48d0ecf1e13afd59b402ab393
crabonfire <kepitingkebakaran@gmail.com>
2025-02-11 07:02:46 +0000

    Submit

---
+++ python_practice/next_year.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+
+#this code is for asking input name and age
+name = input("Enter your name: ")
+print(f"Hello {name}")
+
+age = int(input("Enter your age: "))
+print(f"Hello, {name}! You are {age} years old.")
+
+print(f"Next year you will be {age + 1} years old.") #message to user
